4555c8ad0671f69af208ee3ec0964370418d01d5
ftichy <59052141+ftichy@users.noreply.github.com>
2023-11-14 20:51:21 +0100

    ADD PRODUCT

---
+++ src/ProductCRUD.API/Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductCRUD.API.Models;
+
+namespace ProductCRUD.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductDbContext _context;
+
+        public ProductsController(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            if (_context.Products == null)
+            {
+                return NotFound();
+            }
+            return await _context.Products.ToListAsync();
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<Product>> GetProduct(Guid id)
+        {
+            if (_context.Products == null)
+            {
+                return NotFound();
+            }
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+
+        // POST: api/Products
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(CreateProductRequest request)
+        {
+            if (_context.Products == null)
+            {
+                return Problem("Entity set 'ProductDbContext.Products'  is null.");
+            }
+            var product = Product.Create(request.Name, request.Description, request.Price);
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+        }
+
+    }
+}
